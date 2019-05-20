d1cb8466871a6b2eccb34c9c623284c35b013d7e
Lokathor <zefria@gmail.com>
2019-05-20 01:26:23 -0600

    you can make a window
    Can't handle events yet, but that's soon.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,144 @@
+#![warn(missing_docs)]
+
+//! An opinionated set of "high level" wrappers for the
+//! [fermium](https://github.com/Lokathor/fermium) SDL2 bindings.
+
+use core::{marker::PhantomData, slice::from_raw_parts};
+use fermium::{SDL_WindowFlags::*, *};
+use libc::c_char;
+use phantom_fields::phantom_fields;
+
+/// TODO: version()
+
 /// Obtains the current SDL2 error string.
 ///
 /// You should never need to call this yourself, but I guess you can if you
 /// really want.
 pub fn get_error() -> String {
   unsafe {
-    let base = fermium::SDL_GetError();
-    let len = fermium::SDL_strlen(base);
-    let useful_bytes = core::slice::from_raw_parts(base as *const u8, len);
+    let base = SDL_GetError();
+    let len = SDL_strlen(base);
+    let useful_bytes = from_raw_parts(base as *const u8, len);
     String::from_utf8_lossy(useful_bytes).into_owned()
   }
 }
-
-pub type InitFlags = u32;
-
-#[derive(Debug)]
-pub struct SDLToken;
 
 /// Initializes SDL2 and gives you a token as proof, or an error message.
 ///
 /// # Safety
 ///
-/// You must not double-initialize SDL2. Other than that this is fully safe.
-pub unsafe fn init(flags: InitFlags) -> Result<SDLToken, String> {
-  unimplemented!()
+/// This can only be called from the main thread, and you cannot double
+/// initialize SDL2.
+pub unsafe fn init() -> Result<SDLToken, String> {
+  if SDL_Init(SDL_INIT_EVERYTHING) == 0 {
+    Ok(SDLToken {
+      _marker: PhantomData,
+    })
+  } else {
+    Err(get_error())
+  }
+}
+
+/// The `SDLToken` is proof that you have initialized SDL2.
+///
+/// Most of SDL2 requires you to have performed initialization, and so most of
+/// its abilities are either methods off of this struct or off of things that
+/// you make from methods of this struct.
+#[derive(Debug)]
+pub struct SDLToken {
+  _marker: PhantomData<*mut u8>,
+}
+impl Drop for SDLToken {
+  fn drop(&mut self) {
+    unsafe { SDL_Quit() }
+  }
+}
+#[test]
+fn test_sdl_token_zero_size() {
+  assert_eq!(core::mem::size_of::<SDLToken>(), 0)
+}
+impl SDLToken {
+  /// Creates a new window, or gives an error message.
+  ///
+  /// If no position is specified it's simply centered on screen.
+  pub fn create_window<'sdl>(
+    &'sdl self, title: &str, position: Option<(i32, i32)>, w: i32, h: i32, flags: WindowFlags,
+  ) -> Result<Window<'sdl>, String> {
+    let title_null: Vec<u8> = title.bytes().chain(Some(0)).collect();
+    let (x, y) = position.unwrap_or((WINDOW_POSITION_CENTERED, WINDOW_POSITION_CENTERED));
+    let ptr = unsafe {
+      SDL_CreateWindow(
+        title_null.as_ptr() as *const c_char,
+        x,
+        y,
+        w,
+        h,
+        flags.0 as u32,
+      )
+    };
+    if ptr.is_null() {
+      Err(get_error())
+    } else {
+      Ok(Window {
+        ptr,
+        _marker: PhantomData,
+      })
+    }
+  }
+
+  // TODO: poll_event
+}
+
+/// Flags that a window might have.
+///
+/// This is for use with [create_window](SDLToken::create_window) as well as
+/// other methods that examine the state of a window.
+#[derive(Debug, Default, Clone, Copy)]
+#[repr(transparent)]
+pub struct WindowFlags(i32);
+#[allow(missing_docs)]
+impl WindowFlags {
+  phantom_fields! {
+    self.0: i32,
+    fullscreen: SDL_WINDOW_FULLSCREEN,
+    opengl: SDL_WINDOW_OPENGL,
+    shown: SDL_WINDOW_SHOWN,
+    hidden: SDL_WINDOW_HIDDEN,
+    borderless: SDL_WINDOW_BORDERLESS,
+    resizable: SDL_WINDOW_RESIZABLE,
+    minimized: SDL_WINDOW_MINIMIZED,
+    maximized: SDL_WINDOW_MAXIMIZED,
+    input_grabbed: SDL_WINDOW_INPUT_GRABBED,
+    input_focus: SDL_WINDOW_INPUT_FOCUS,
+    mouse_focus: SDL_WINDOW_MOUSE_FOCUS,
+    fullscreen_desktop: SDL_WINDOW_FULLSCREEN_DESKTOP,
+    foreign: SDL_WINDOW_FOREIGN,
+    /// Window should be created in high-DPI mode if supported.
+    ///
+    /// On macOS `NSHighResolutionCapable` must be set true in the application's
+    /// `Info.plist` for this to have any effect.
+    allow_high_dpi: SDL_WINDOW_ALLOW_HIGHDPI,
+    /// Distinct from "input grabbed".
+    mouse_capture: SDL_WINDOW_MOUSE_CAPTURE,
+    always_on_top: SDL_WINDOW_ALWAYS_ON_TOP,
+    /// Window should not be added to the taskbar list (eg: a dialog box).
+    skip_taskbar: SDL_WINDOW_SKIP_TASKBAR,
+    utility: SDL_WINDOW_UTILITY,
+    tooltip: SDL_WINDOW_TOOLTIP,
+    popup_menu: SDL_WINDOW_POPUP_MENU,
+    vulkan: SDL_WINDOW_VULKAN,
+  }
+}
+
+/// Centers the window along the axis used.
+///
+/// See [create_window](SDLToken::create_window).
+pub const WINDOW_POSITION_CENTERED: i32 = SDL_WINDOWPOS_CENTERED_MASK as i32;
+
+/// Handle to a window on the screen.
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct Window<'sdl> {
+  ptr: *mut SDL_Window,
+  _marker: PhantomData<&'sdl SDLToken>,
 }
