5b672b799ce2dea8e4a74120da405eb5885948d4
Lokathor <zefria@gmail.com>
2019-05-26 21:24:10 -0600

    pixels and buttons

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@
 //! [fermium](https://github.com/Lokathor/fermium) SDL2 bindings.
 
 use core::{marker::PhantomData, ptr::null_mut, slice::from_raw_parts};
-use fermium::{SDL_EventType::*, SDL_RendererFlags::*, SDL_WindowFlags::*, *};
+use fermium::{
+  SDL_EventType::*, SDL_RendererFlags::*, SDL_WindowFlags::*, _bindgen_ty_1::*, _bindgen_ty_2::*,
+  _bindgen_ty_3::*, _bindgen_ty_4::*, _bindgen_ty_5::*, _bindgen_ty_6::*, *,
+};
 use libc::c_char;
 use phantom_fields::phantom_fields;
 
@@ -550,8 +553,59 @@ impl<'sdl, 'win, 'ren> Drop for Texture<'sdl, 'win, 'ren> {
   }
 }
 
-pub type MouseButtonState = u32;
-pub type MouseButton = u8;
+macro_rules! sdl_button {
+  ($x:expr) => {
+    1 << ($x - 1)
+  };
+}
+const SDL_BUTTON_LMASK: u32 = sdl_button!(SDL_BUTTON_LEFT);
+const SDL_BUTTON_MMASK: u32 = sdl_button!(SDL_BUTTON_MIDDLE);
+const SDL_BUTTON_RMASK: u32 = sdl_button!(SDL_BUTTON_RIGHT);
+const SDL_BUTTON_X1MASK: u32 = sdl_button!(SDL_BUTTON_X1);
+const SDL_BUTTON_X2MASK: u32 = sdl_button!(SDL_BUTTON_X2);
+
+/// Holds flags for the state of all mouse buttons at any given moment.
+#[derive(Debug, Clone, Copy)]
+pub struct MouseButtonState(u32);
+impl MouseButtonState {
+  phantom_fields! {
+    self.0: u32,
+    left: SDL_BUTTON_LMASK,
+    middle: SDL_BUTTON_MMASK,
+    right: SDL_BUTTON_RMASK,
+    x1: SDL_BUTTON_X1MASK,
+    x2: SDL_BUTTON_X2MASK,
+  }
+}
+
+/// The possible mouse buttons.
+#[derive(Debug, Clone, Copy)]
+pub enum MouseButton {
+  /// Left side
+  Left,
+  /// Middle, usually a mouse wheel click
+  Middle,
+  /// Right side
+  Right,
+  /// Extra button 1
+  X1,
+  /// Extra button 2
+  X2,
+  /// Unknown mouse button
+  Unknown,
+}
+impl From<u8> for MouseButton {
+  fn from(button_byte: u8) -> Self {
+    match u32::from(button_byte) {
+      SDL_BUTTON_LEFT => MouseButton::Left,
+      SDL_BUTTON_MIDDLE => MouseButton::Middle,
+      SDL_BUTTON_RIGHT => MouseButton::Right,
+      SDL_BUTTON_X1 => MouseButton::X1,
+      SDL_BUTTON_X2 => MouseButton::X1,
+      _ => MouseButton::Unknown,
+    }
+  }
+}
 
 /// The various events that can happen.
 #[derive(Debug, Clone, Copy)]
@@ -638,7 +692,7 @@ impl From<SDL_Event> for Event {
           } else {
             Some(event.motion.which)
           },
-          state: event.motion.state,
+          state: MouseButtonState(event.motion.state),
           x: event.motion.x,
           y: event.motion.y,
           delta_x: event.motion.xrel,
@@ -652,7 +706,7 @@ impl From<SDL_Event> for Event {
           } else {
             Some(event.button.which)
           },
-          button: event.button.button,
+          button: MouseButton::from(event.button.button),
           is_pressed: u32::from(event.button.state) == SDL_PRESSED,
           clicks: event.button.clicks,
           x: event.button.x,
@@ -722,4 +776,364 @@ impl From<SDL_Rect> for Rect {
       h: other.h,
     }
   }
+}
+
+/// The various pixel formats that SDL2 supports.
+///
+/// There's various checks you can perform on each pixel format.
+///
+/// Note that the "fourcc" formats, anything that gives `true` from the
+/// [is_fourcc](PixelFormat:is_fourcc) method, are industry specified special
+/// values, and do not follow SDL2's bit packing scheme. In other words, the
+/// output they produce for any of the other check methods is not to really be
+/// trusted.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum PixelFormat {
+  Unknown = SDL_PIXELFORMAT_UNKNOWN,
+  Index1lsb = SDL_PIXELFORMAT_INDEX1LSB,
+  Index1msb = SDL_PIXELFORMAT_INDEX1MSB,
+  Index4lsb = SDL_PIXELFORMAT_INDEX4LSB,
+  Index4msb = SDL_PIXELFORMAT_INDEX4MSB,
+  Index8 = SDL_PIXELFORMAT_INDEX8,
+  RGB332 = SDL_PIXELFORMAT_RGB332,
+  RGB444 = SDL_PIXELFORMAT_RGB444,
+  RGB555 = SDL_PIXELFORMAT_RGB555,
+  BGR555 = SDL_PIXELFORMAT_BGR555,
+  ARGB4444 = SDL_PIXELFORMAT_ARGB4444,
+  RGBA4444 = SDL_PIXELFORMAT_RGBA4444,
+  ABGR4444 = SDL_PIXELFORMAT_ABGR4444,
+  BGRA4444 = SDL_PIXELFORMAT_BGRA4444,
+  ARGB1555 = SDL_PIXELFORMAT_ARGB1555,
+  RGBA5551 = SDL_PIXELFORMAT_RGBA5551,
+  ABGR1555 = SDL_PIXELFORMAT_ABGR1555,
+  BGRA5551 = SDL_PIXELFORMAT_BGRA5551,
+  RGB565 = SDL_PIXELFORMAT_RGB565,
+  BGR565 = SDL_PIXELFORMAT_BGR565,
+  RGB24 = SDL_PIXELFORMAT_RGB24,
+  BGR24 = SDL_PIXELFORMAT_BGR24,
+  RGB888 = SDL_PIXELFORMAT_RGB888,
+  RGBX8888 = SDL_PIXELFORMAT_RGBX8888,
+  BGR888 = SDL_PIXELFORMAT_BGR888,
+  BGRX8888 = SDL_PIXELFORMAT_BGRX8888,
+  ARGB8888 = SDL_PIXELFORMAT_ARGB8888,
+  RGBA8888 = SDL_PIXELFORMAT_RGBA8888,
+  ABGR8888 = SDL_PIXELFORMAT_ABGR8888,
+  BGRA8888 = SDL_PIXELFORMAT_BGRA8888,
+  ARGB2101010 = SDL_PIXELFORMAT_ARGB2101010,
+  /// Planar mode: Y + V + U (3 planes)
+  YV12 = SDL_PIXELFORMAT_YV12,
+  /// Planar mode: Y + U + V (3 planes)
+  IYUV = SDL_PIXELFORMAT_IYUV,
+  /// Packed mode: Y0+U0+Y1+V0 (1 plane)
+  YUY2 = SDL_PIXELFORMAT_YUY2,
+  /// Packed mode: U0+Y0+V0+Y1 (1 plane)
+  UYVY = SDL_PIXELFORMAT_UYVY,
+  /// Packed mode: Y0+V0+Y1+U0 (1 plane)
+  YVYU = SDL_PIXELFORMAT_YVYU,
+  /// Planar mode: Y + U/V interleaved (2 planes)
+  NV12 = SDL_PIXELFORMAT_NV12,
+  /// Planar mode: Y + V/U interleaved (2 planes)
+  NV21 = SDL_PIXELFORMAT_NV21,
+  /// Android video texture format
+  ExternalOES = SDL_PIXELFORMAT_EXTERNAL_OES,
+}
+#[cfg(target_endian = "big")]
+impl PixelFormat {
+  /// Platform specific alias for RGBA
+  pub const RGBA32: Self = PixelFormat::RGBA8888;
+  /// Platform specific alias for ARGB
+  pub const ARGB32: Self = PixelFormat::ARGB8888;
+  /// Platform specific alias for BGRA
+  pub const BGRA32: Self = PixelFormat::BGRA8888;
+  /// Platform specific alias for ABGR
+  pub const ABGR32: Self = PixelFormat::ABGR8888;
+}
+#[cfg(target_endian = "little")]
+impl PixelFormat {
+  /// Platform specific alias for RGBA
+  pub const RGBA32: Self = PixelFormat::ABGR8888;
+  /// Platform specific alias for ARGB
+  pub const ARGB32: Self = PixelFormat::BGRA8888;
+  /// Platform specific alias for BGRA
+  pub const BGRA32: Self = PixelFormat::ARGB8888;
+  /// Platform specific alias for ABGR
+  pub const ABGR32: Self = PixelFormat::RGBA8888;
+}
+impl From<fermium::_bindgen_ty_6::Type> for PixelFormat {
+  fn from(pf: fermium::_bindgen_ty_6::Type) -> Self {
+    match pf {
+      SDL_PIXELFORMAT_INDEX1LSB => PixelFormat::Index1lsb,
+      SDL_PIXELFORMAT_INDEX1MSB => PixelFormat::Index1msb,
+      SDL_PIXELFORMAT_INDEX4LSB => PixelFormat::Index4lsb,
+      SDL_PIXELFORMAT_INDEX4MSB => PixelFormat::Index4msb,
+      SDL_PIXELFORMAT_INDEX8 => PixelFormat::Index8,
+      SDL_PIXELFORMAT_RGB332 => PixelFormat::RGB332,
+      SDL_PIXELFORMAT_RGB444 => PixelFormat::RGB444,
+      SDL_PIXELFORMAT_RGB555 => PixelFormat::RGB555,
+      SDL_PIXELFORMAT_BGR555 => PixelFormat::BGR555,
+      SDL_PIXELFORMAT_ARGB4444 => PixelFormat::ARGB4444,
+      SDL_PIXELFORMAT_RGBA4444 => PixelFormat::RGBA4444,
+      SDL_PIXELFORMAT_ABGR4444 => PixelFormat::ABGR4444,
+      SDL_PIXELFORMAT_BGRA4444 => PixelFormat::BGRA4444,
+      SDL_PIXELFORMAT_ARGB1555 => PixelFormat::ARGB1555,
+      SDL_PIXELFORMAT_RGBA5551 => PixelFormat::RGBA5551,
+      SDL_PIXELFORMAT_ABGR1555 => PixelFormat::ABGR1555,
+      SDL_PIXELFORMAT_BGRA5551 => PixelFormat::BGRA5551,
+      SDL_PIXELFORMAT_RGB565 => PixelFormat::RGB565,
+      SDL_PIXELFORMAT_BGR565 => PixelFormat::BGR565,
+      SDL_PIXELFORMAT_RGB24 => PixelFormat::RGB24,
+      SDL_PIXELFORMAT_BGR24 => PixelFormat::BGR24,
+      SDL_PIXELFORMAT_RGB888 => PixelFormat::RGB888,
+      SDL_PIXELFORMAT_RGBX8888 => PixelFormat::RGBX8888,
+      SDL_PIXELFORMAT_BGR888 => PixelFormat::BGR888,
+      SDL_PIXELFORMAT_BGRX8888 => PixelFormat::BGRX8888,
+      SDL_PIXELFORMAT_ARGB8888 => PixelFormat::ARGB8888,
+      SDL_PIXELFORMAT_RGBA8888 => PixelFormat::RGBA8888,
+      SDL_PIXELFORMAT_ABGR8888 => PixelFormat::ABGR8888,
+      SDL_PIXELFORMAT_BGRA8888 => PixelFormat::BGRA8888,
+      SDL_PIXELFORMAT_ARGB2101010 => PixelFormat::ARGB2101010,
+      SDL_PIXELFORMAT_YV12 => PixelFormat::YV12,
+      SDL_PIXELFORMAT_IYUV => PixelFormat::IYUV,
+      SDL_PIXELFORMAT_YUY2 => PixelFormat::YUY2,
+      SDL_PIXELFORMAT_UYVY => PixelFormat::UYVY,
+      SDL_PIXELFORMAT_YVYU => PixelFormat::YVYU,
+      SDL_PIXELFORMAT_NV12 => PixelFormat::NV12,
+      SDL_PIXELFORMAT_NV21 => PixelFormat::NV21,
+      SDL_PIXELFORMAT_EXTERNAL_OES => PixelFormat::ExternalOES,
+      _ => PixelFormat::Unknown,
+    }
+  }
+}
+impl PixelFormat {
+  /// The type of the pixel format.
+  ///
+  /// All unknown types convert to `PixelType::Unknown`, of course.
+  pub fn pixel_type(self) -> PixelType {
+    match ((self as u32 >> 24) & 0x0F) as fermium::_bindgen_ty_1::Type {
+      SDL_PIXELTYPE_INDEX1 => PixelType::Index1,
+      SDL_PIXELTYPE_INDEX4 => PixelType::Index4,
+      SDL_PIXELTYPE_INDEX8 => PixelType::Index8,
+      SDL_PIXELTYPE_PACKED8 => PixelType::Packed8,
+      SDL_PIXELTYPE_PACKED16 => PixelType::Packed16,
+      SDL_PIXELTYPE_PACKED32 => PixelType::Packed32,
+      SDL_PIXELTYPE_ARRAYU8 => PixelType::ArrayU8,
+      SDL_PIXELTYPE_ARRAYU16 => PixelType::ArrayU16,
+      SDL_PIXELTYPE_ARRAYU32 => PixelType::ArrayU32,
+      SDL_PIXELTYPE_ARRAYF16 => PixelType::ArrayF16,
+      SDL_PIXELTYPE_ARRAYF32 => PixelType::ArrayF32,
+      _ => PixelType::Unknown,
+    }
+  }
+
+  /// Ordering of channel or bits in the pixel format.
+  ///
+  /// Unknown values convert to one of the `None` variants.
+  pub fn pixel_order(self) -> PixelOrder {
+    let bits = (self as u32 >> 20) & 0x0F;
+    if self.is_packed() {
+      match bits as fermium::_bindgen_ty_4::Type {
+        SDL_PACKEDORDER_ABGR => PixelOrder::Packed(PackedPixelOrder::ABGR),
+        SDL_PACKEDORDER_ARGB => PixelOrder::Packed(PackedPixelOrder::ARGB),
+        SDL_PACKEDORDER_BGRA => PixelOrder::Packed(PackedPixelOrder::BGRA),
+        SDL_PACKEDORDER_BGRX => PixelOrder::Packed(PackedPixelOrder::BGRX),
+        SDL_PACKEDORDER_RGBA => PixelOrder::Packed(PackedPixelOrder::RGBA),
+        SDL_PACKEDORDER_RGBX => PixelOrder::Packed(PackedPixelOrder::RGBX),
+        SDL_PACKEDORDER_XBGR => PixelOrder::Packed(PackedPixelOrder::XBGR),
+        SDL_PACKEDORDER_XRGB => PixelOrder::Packed(PackedPixelOrder::XRGB),
+        _ => PixelOrder::Packed(PackedPixelOrder::None),
+      }
+    } else if self.is_array() {
+      match bits as fermium::_bindgen_ty_4::Type {
+        SDL_ARRAYORDER_ABGR => PixelOrder::Array(ArrayPixelOrder::ABGR),
+        SDL_ARRAYORDER_ARGB => PixelOrder::Array(ArrayPixelOrder::ARGB),
+        SDL_ARRAYORDER_BGR => PixelOrder::Array(ArrayPixelOrder::BGR),
+        SDL_ARRAYORDER_BGRA => PixelOrder::Array(ArrayPixelOrder::BGRA),
+        SDL_ARRAYORDER_RGB => PixelOrder::Array(ArrayPixelOrder::RGB),
+        SDL_ARRAYORDER_RGBA => PixelOrder::Array(ArrayPixelOrder::RGBA),
+        _ => PixelOrder::Array(ArrayPixelOrder::None),
+      }
+    } else {
+      match bits as fermium::_bindgen_ty_2::Type {
+        SDL_BITMAPORDER_1234 => PixelOrder::Bitmap(BitmapPixelOrder::_1234),
+        SDL_BITMAPORDER_4321 => PixelOrder::Bitmap(BitmapPixelOrder::_4321),
+        _ => PixelOrder::Bitmap(BitmapPixelOrder::None),
+      }
+    }
+  }
+
+  /// Channel bits pattern of the pixel format.
+  ///
+  /// Converts any possible unknown layout to `PixelLayout::None`.
+  pub fn pixel_layout(self) -> PixelLayout {
+    match ((self as u32 >> 16) & 0x0F) as fermium::_bindgen_ty_1::Type {
+      SDL_PACKEDLAYOUT_332 => PixelLayout::_332,
+      SDL_PACKEDLAYOUT_4444 => PixelLayout::_4444,
+      SDL_PACKEDLAYOUT_1555 => PixelLayout::_1555,
+      SDL_PACKEDLAYOUT_5551 => PixelLayout::_5551,
+      SDL_PACKEDLAYOUT_565 => PixelLayout::_565,
+      SDL_PACKEDLAYOUT_8888 => PixelLayout::_8888,
+      SDL_PACKEDLAYOUT_2101010 => PixelLayout::_2101010,
+      SDL_PACKEDLAYOUT_1010102 => PixelLayout::_1010102,
+      _ => PixelLayout::None,
+    }
+  }
+
+  /// Bits of color information per pixel.
+  pub fn bits_per_pixel(self) -> u32 {
+    (self as u32 >> 8) & 0xFF
+  }
+
+  /// Bytes used per pixel.
+  ///
+  /// Note: Formats with less than 8 bits per pixel give a result of 0 bytes per
+  /// pixel. Weird and all, but that's how it is.
+  pub fn bytes_per_pixel(self) -> u32 {
+    if self.is_fourcc() {
+      match self {
+        PixelFormat::YUY2 | PixelFormat::UYVY | PixelFormat::YVYU => 2,
+        _ => 1,
+      }
+    } else {
+      self as u32 & 0xFF
+    }
+  }
+
+  /// Is this format an indexed format?
+  pub fn is_indexed(self) -> bool {
+    !self.is_fourcc()
+      && match self.pixel_type() {
+        PixelType::Index1 | PixelType::Index4 | PixelType::Index8 => true,
+        _ => false,
+      }
+  }
+
+  /// Is this format a packed format?
+  pub fn is_packed(self) -> bool {
+    !self.is_fourcc()
+      && match self.pixel_type() {
+        PixelType::Packed8 | PixelType::Packed16 | PixelType::Packed32 => true,
+        _ => false,
+      }
+  }
+
+  /// Is this format a packed format?
+  pub fn is_array(self) -> bool {
+    !self.is_fourcc()
+      && match self.pixel_type() {
+        PixelType::ArrayU8
+        | PixelType::ArrayU16
+        | PixelType::ArrayU32
+        | PixelType::ArrayF16
+        | PixelType::ArrayF32 => true,
+        _ => false,
+      }
+  }
+
+  /// Is this format a format with an alpha channel?
+  pub fn is_alpha(self) -> bool {
+    match self.pixel_order() {
+      PixelOrder::Packed(PackedPixelOrder::ARGB)
+      | PixelOrder::Packed(PackedPixelOrder::RGBA)
+      | PixelOrder::Packed(PackedPixelOrder::ABGR)
+      | PixelOrder::Packed(PackedPixelOrder::BGRA)
+      | PixelOrder::Array(ArrayPixelOrder::ARGB)
+      | PixelOrder::Array(ArrayPixelOrder::RGBA)
+      | PixelOrder::Array(ArrayPixelOrder::ABGR)
+      | PixelOrder::Array(ArrayPixelOrder::BGRA) => true,
+      _ => false,
+    }
+  }
+
+  /// Is this a [four-character code](https://en.wikipedia.org/wiki/FourCC) format?
+  ///
+  /// True for pixel formats representing unique formats, for example YUV formats.
+  pub fn is_fourcc(self) -> bool {
+    (self as u32 > 0) && (((self as u32 >> 28) & 0x0F) != 1)
+  }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum PixelType {
+  Unknown = SDL_PIXELTYPE_UNKNOWN,
+  Index1 = SDL_PIXELTYPE_INDEX1,
+  Index4 = SDL_PIXELTYPE_INDEX4,
+  Index8 = SDL_PIXELTYPE_INDEX8,
+  Packed8 = SDL_PIXELTYPE_PACKED8,
+  Packed16 = SDL_PIXELTYPE_PACKED16,
+  Packed32 = SDL_PIXELTYPE_PACKED32,
+  ArrayU8 = SDL_PIXELTYPE_ARRAYU8,
+  ArrayU16 = SDL_PIXELTYPE_ARRAYU16,
+  ArrayU32 = SDL_PIXELTYPE_ARRAYU32,
+  ArrayF16 = SDL_PIXELTYPE_ARRAYF16,
+  ArrayF32 = SDL_PIXELTYPE_ARRAYF32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[allow(missing_docs)]
+pub enum PixelOrder {
+  Bitmap(BitmapPixelOrder),
+  Packed(PackedPixelOrder),
+  Array(ArrayPixelOrder),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum BitmapPixelOrder {
+  None = SDL_BITMAPORDER_NONE,
+  _4321 = SDL_BITMAPORDER_4321,
+  _1234 = SDL_BITMAPORDER_1234,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum PackedPixelOrder {
+  None = SDL_PACKEDORDER_NONE,
+  XRGB = SDL_PACKEDORDER_XRGB,
+  RGBX = SDL_PACKEDORDER_RGBX,
+  ARGB = SDL_PACKEDORDER_ARGB,
+  RGBA = SDL_PACKEDORDER_RGBA,
+  XBGR = SDL_PACKEDORDER_XBGR,
+  BGRX = SDL_PACKEDORDER_BGRX,
+  ABGR = SDL_PACKEDORDER_ABGR,
+  BGRA = SDL_PACKEDORDER_BGRA,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum ArrayPixelOrder {
+  None = SDL_ARRAYORDER_NONE,
+  RGB = SDL_ARRAYORDER_RGB,
+  RGBA = SDL_ARRAYORDER_RGBA,
+  ARGB = SDL_ARRAYORDER_ARGB,
+  BGR = SDL_ARRAYORDER_BGR,
+  BGRA = SDL_ARRAYORDER_BGRA,
+  ABGR = SDL_ARRAYORDER_ABGR,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(not(windows), repr(u32))]
+#[allow(missing_docs)]
+pub enum PixelLayout {
+  None = SDL_PACKEDLAYOUT_NONE,
+  _332 = SDL_PACKEDLAYOUT_332,
+  _4444 = SDL_PACKEDLAYOUT_4444,
+  _1555 = SDL_PACKEDLAYOUT_1555,
+  _5551 = SDL_PACKEDLAYOUT_5551,
+  _565 = SDL_PACKEDLAYOUT_565,
+  _8888 = SDL_PACKEDLAYOUT_8888,
+  _2101010 = SDL_PACKEDLAYOUT_2101010,
+  _1010102 = SDL_PACKEDLAYOUT_1010102,
 }
---
+++ tests/pixel_format.rs
@@ -0,0 +1,180 @@
+
+use beryllium::*;
+
+#[rustfmt::skip]
+#[test]
+pub fn test_compare_with_header_definitions() {
+  fn basics_flags(format: PixelFormat) -> ((PixelType, PixelOrder, PixelLayout, u32, u32), (bool, bool, bool, bool)) {
+    (
+      (format.pixel_type(),format.pixel_order(),format.pixel_layout(),format.bits_per_pixel(),format.bytes_per_pixel()),
+      (format.is_indexed(),format.is_packed(),format.is_array(),format.is_fourcc())
+    )
+  }
+  //
+  let format = PixelFormat::Unknown;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Unknown, PixelOrder::Bitmap(BitmapPixelOrder::None), PixelLayout::None, 0, 0));
+  assert_eq!(flags, (false, false, false, false));
+  //
+  let format = PixelFormat::Index1lsb;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Index1, PixelOrder::Bitmap(BitmapPixelOrder::_4321), PixelLayout::None, 1, 0));
+  assert_eq!(flags, (true, false, false, false));
+  //
+  let format = PixelFormat::Index1msb;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Index1, PixelOrder::Bitmap(BitmapPixelOrder::_1234), PixelLayout::None, 1, 0));
+  assert_eq!(flags, (true, false, false, false));
+  //
+  let format = PixelFormat::Index4lsb;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Index4, PixelOrder::Bitmap(BitmapPixelOrder::_4321), PixelLayout::None, 4, 0));
+  assert_eq!(flags, (true, false, false, false));
+  //
+  let format = PixelFormat::Index4msb;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Index4, PixelOrder::Bitmap(BitmapPixelOrder::_1234), PixelLayout::None, 4, 0));
+  assert_eq!(flags, (true, false, false, false));
+  //
+  let format = PixelFormat::Index8;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Index8, PixelOrder::Bitmap(BitmapPixelOrder::None), PixelLayout::None, 8, 1));
+  assert_eq!(flags, (true, false, false, false));
+  //
+  let format = PixelFormat::RGB332;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed8, PixelOrder::Packed(PackedPixelOrder::XRGB), PixelLayout::_332, 8, 1));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGB444;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::XRGB), PixelLayout::_4444, 12, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGB555;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::XRGB), PixelLayout::_1555, 15, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGR555;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::XBGR), PixelLayout::_1555, 15, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ARGB4444;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::ARGB), PixelLayout::_4444, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGBA4444;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::RGBA), PixelLayout::_4444, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ABGR4444;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::ABGR), PixelLayout::_4444, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGRA4444;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::BGRA), PixelLayout::_4444, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ARGB1555;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::ARGB), PixelLayout::_1555, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGBA5551;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::RGBA), PixelLayout::_5551, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ABGR1555;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::ABGR), PixelLayout::_1555, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGRA5551;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::BGRA), PixelLayout::_5551, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGB565;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::XRGB), PixelLayout::_565, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGR565;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed16, PixelOrder::Packed(PackedPixelOrder::XBGR), PixelLayout::_565, 16, 2));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGB24;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::ArrayU8, PixelOrder::Array(ArrayPixelOrder::RGB), PixelLayout::None, 24, 3));
+  assert_eq!(flags, (false, false, true, false));
+  //
+  let format = PixelFormat::BGR24;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::ArrayU8, PixelOrder::Array(ArrayPixelOrder::BGR), PixelLayout::None, 24, 3));
+  assert_eq!(flags, (false, false, true, false));
+  //
+  let format = PixelFormat::RGB888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::XRGB), PixelLayout::_8888, 24, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGBX8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::RGBX), PixelLayout::_8888, 24, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGR888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::XBGR), PixelLayout::_8888, 24, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGRX8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::BGRX), PixelLayout::_8888, 24, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ARGB8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::ARGB), PixelLayout::_8888, 32, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::RGBA8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::RGBA), PixelLayout::_8888, 32, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ABGR8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::ABGR), PixelLayout::_8888, 32, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::BGRA8888;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::BGRA), PixelLayout::_8888, 32, 4));
+  assert_eq!(flags, (false, true, false, false));
+  //
+  let format = PixelFormat::ARGB2101010;
+  let (basics, flags) = basics_flags(format);
+  assert_eq!(basics, (PixelType::Packed32, PixelOrder::Packed(PackedPixelOrder::ARGB), PixelLayout::_2101010, 32, 4));
+  assert_eq!(flags, (false, true, false, false));
+  
+  // The fourcc pixel formats are standards based values that don't follow
+  // SDL2's bit packing scheme, so they basically have nonsense as their
+  // "basics" entries, however, the fourcc flag result must always be correct.
+  assert!(PixelFormat::YV12.is_fourcc());
+  assert!(PixelFormat::IYUV.is_fourcc());
+  assert!(PixelFormat::YUY2.is_fourcc());
+  assert!(PixelFormat::UYVY.is_fourcc());
+  assert!(PixelFormat::YVYU.is_fourcc());
+  assert!(PixelFormat::NV12.is_fourcc());
+  assert!(PixelFormat::NV21.is_fourcc());
+  assert!(PixelFormat::ExternalOES.is_fourcc());
+}
