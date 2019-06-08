02f5b29ae41380de368ccba63207d95b04980b59
Lokathor <zefria@gmail.com>
2019-06-08 15:02:03 -0600

    gl cleanup, start of glium

--- examples/extern_crate_glium.rs
+++ examples/extern_crate_glium.rs
@@ -3,13 +3,21 @@
 //! Demo of how to setup a blank window using the `gl` crate for OpenGL.
 
 use beryllium::*;
-use gl;
+use glium;
+
+use core::convert::TryFrom;
+use core::ops::Deref;
 
 fn main() -> Result<(), String> {
-  // Init SDL2
+  // init SDL2
   let sdl = unsafe { beryllium::init() }?;
 
-  // Make a window (include the flag for OpenGL support!)
+  // Set our context request settings
+  sdl.gl_set_attribute(GLattr::ContextProfileMask, CONTEXT_PROFILE_CORE);
+  sdl.gl_set_attribute(GLattr::ContextMajorVersion, 3);
+  sdl.gl_set_attribute(GLattr::ContextMinorVersion, 3);
+
+  // make a window and then a context (which is automatically made current)
   let window = sdl.create_window(
     "Extern Crate: `gl`",                                      // title
     WINDOW_POSITION_CENTERED,                                  // x
@@ -18,14 +26,9 @@ fn main() -> Result<(), String> {
     600,                                                       // height
     WindowFlags::default().with_shown(true).with_opengl(true), // flags
   )?;
-
-  // Set our context request settings and make a context
-  sdl.gl_set_attribute(GLattr::ContextProfileMask, CONTEXT_PROFILE_CORE);
-  sdl.gl_set_attribute(GLattr::ContextMajorVersion, 3);
-  sdl.gl_set_attribute(GLattr::ContextMinorVersion, 3);
   let _ctx = unsafe { window.gl_create_context()? };
 
-  // ONCE WE HAVE A CONTEXT (not before!) we can load up the OpenGL functions
+  // ONCE WE HAVE A CONTEXT we can load up OpenGL (not before!)
   gl::load_with(|s| unsafe { sdl.gl_get_proc_address(s) });
 
   unsafe { gl::ClearColor(1.0, 0.5, 0.0, 1.0) };
@@ -50,4 +53,18 @@ fn main() -> Result<(), String> {
   }
 
   Ok(())
+}
+
+pub struct GliumWindow<'sdl>(Window<'sdl>);
+impl<'sdl> Deref for GliumWindow<'sdl> {
+  type Target = Window<'sdl>;
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+impl<'sdl> TryFrom<Window<'sdl>> for GliumWindow<'sdl> {
+  type Error = String;
+  fn try_from(win: Window) -> Result<Self, Self::Error> {
+    unimplemented!()
+  }
 }
