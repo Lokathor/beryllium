ccdd68fba29dbb13a1f2849cddc2fc7538a1fdfb
Lokathor <zefria@gmail.com>
2019-06-15 15:31:46 -0600

    Update joystick_check.rs

--- examples/joystick_check.rs
+++ examples/joystick_check.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), String> {
   let joystick_count = sdl.number_of_joysticks()?;
   if joystick_count > 0 {
     for index in 0..joystick_count {
-      if sdl.joystick_is_game_controller(index) {
+      if sdl.joystick_is_game_controller(JoystickID(index)) {
         match sdl.controller_name(index) {
           Some(name) => println!("Joystick #{} has controller name: {}", index, name),
           None => println!("Joystick #{} is a controller with no name", index),
