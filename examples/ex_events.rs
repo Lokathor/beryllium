26dca0de7cb6bbb4b5129fa42f83b4b2bc364b3a
Lokathor <zefria@gmail.com>
2020-09-13 03:04:15 -0600

    use controllers during event test

--- examples/ex_events.rs
+++ examples/ex_events.rs
@@ -10,6 +10,11 @@ fn main() {
       WindowCreationFlags::default(),
     )
     .unwrap();
+  let mut controllers = vec![];
+  let joystick_count = sdl.get_number_of_joysticks().unwrap();
+  for n in 0 .. joystick_count {
+    controllers.push(sdl.open_controller(n).unwrap())
+  }
   'main: loop {
     while let Some((event, time)) = sdl.poll_event() {
       println!("[{time}] {event:?}", time = time, event = event);
